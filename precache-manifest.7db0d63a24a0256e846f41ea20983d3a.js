5086ea446dfeea858dc86eaa03424c3d129c2b6a
phurinaix <phurinaix@gmail.com>
2019-05-15 00:04:34 +0700

    Updates

---
+++ precache-manifest.7db0d63a24a0256e846f41ea20983d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4738a1dddc8e5c59498bad240eb831c2",
+    "url": "/blockchain/index.html"
+  },
+  {
+    "revision": "5f2eb55e85be063f125c",
+    "url": "/blockchain/static/css/main.37ccd69d.chunk.css"
+  },
+  {
+    "revision": "e546335a5016ce750dfd",
+    "url": "/blockchain/static/js/2.deee6eea.chunk.js"
+  },
+  {
+    "revision": "5f2eb55e85be063f125c",
+    "url": "/blockchain/static/js/main.b153e7f9.chunk.js"
+  },
+  {
+    "revision": "178f2a6adc8ee9b57b76",
+    "url": "/blockchain/static/js/runtime~main.07ddac04.js"
+  }
+]);
