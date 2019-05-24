bcbe0ee92d84d5361c4c963a9a59844b90bdc9f6
phurinaix <phurinaix@gmail.com>
2019-05-25 01:28:03 +0700

    Updates

--- precache-manifest.3a7c792527e88390171c3b27e43ca751.js
+++ precache-manifest.3a7c792527e88390171c3b27e43ca751.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dea594471e051750cde8be83e00efac8",
+    "revision": "0be3fe4d2ea2251dbdde42ebf3232551",
     "url": "/blockchain-project/index.html"
   },
   {
-    "revision": "d5dafd978754d33781c0",
+    "revision": "a23d17420d626e5b748e",
     "url": "/blockchain-project/static/css/main.f55a9e43.chunk.css"
   },
   {
-    "revision": "3f180a7e38d11b7e8d9b",
-    "url": "/blockchain-project/static/js/2.18098bba.chunk.js"
+    "revision": "12a33d9ae0e9c2ab632d",
+    "url": "/blockchain-project/static/js/2.effbffd1.chunk.js"
   },
   {
-    "revision": "d5dafd978754d33781c0",
-    "url": "/blockchain-project/static/js/main.e2242684.chunk.js"
+    "revision": "a23d17420d626e5b748e",
+    "url": "/blockchain-project/static/js/main.33a1bf20.chunk.js"
   },
   {
     "revision": "6c7ee7ad7c7db0c8bf26",
