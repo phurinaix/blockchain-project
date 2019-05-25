92b75cd3b8e633c68036dd902949992ba02a37c9
phurinat <phurinaix@gmail.com>
2019-05-25 23:49:56 +0700

    update add issuer

--- src/containers/Add.js
+++ src/containers/Add.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import NavigationInside from '../components/NavigationInside';
 import AddIssuerInstruction from '../components/AddIssuerInstruction';
+import { Spinner } from 'react-bootstrap';
 import EmailForm from '../components/EmailForm';
 import axios from 'axios';
 
 class Add extends Component {
     state = {
-        isGiveEmail: false
+        isGiveEmail: false,
+        fetching: false
     }
     isGiveEmailHandle = () => {
         this.setState({ isGiveEmail: true });
     };
     componentDidMount = () => {
+        this.setState({ fetching: true });
         axios.post('https://tu-issuer.herokuapp.com/recipient/session', {
                 key: localStorage.getItem("key0")
             })
@@ -25,7 +28,10 @@ class Add extends Component {
                         .then(response => {
                             console.log(response);
                             if (response.data) {
-                                this.setState({ isGiveEmail: true });
+                                this.setState({ 
+                                    isGiveEmail: true,
+                                    fetching: false
+                                });
                             }
                         })
                         .catch(err => {
@@ -42,14 +48,24 @@ class Add extends Component {
             <React.Fragment>
                 <NavigationInside />
                 <div className="mb-5">
-                    <h3 className="text-center">Add Issuer</h3>
-                        {this.state.isGiveEmail ?
-                            <AddIssuerInstruction />
-                        :
-                            <div className="text-center">
-                                <h5>You have to provide an email before add issuer</h5>
-                                <EmailForm email={this.isGiveEmailHandle} buttonText="Submit"/>
+                        { this.state.fetching ? 
+                            <div className="text-center mt-5">
+                                <br/><br/><br/>
+                                <br/><br/><br/>
+                                <Spinner animation="border"role="status"><span className="sr-only">Loading...</span></Spinner>
                             </div>
+                        :
+                            <React.Fragment>
+                                <h3 className="text-center">Add Issuer</h3>
+                                {this.state.isGiveEmail ?
+                                    <AddIssuerInstruction />
+                                :
+                                    <div className="text-center">
+                                        <h5>You have to provide an email before add issuer</h5>
+                                        <EmailForm email={this.isGiveEmailHandle} buttonText="Submit"/>
+                                    </div>
+                                }
+                            </React.Fragment>
                         }
                 </div>
             </React.Fragment>
